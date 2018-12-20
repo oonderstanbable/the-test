1167a7e29aca5b1c75d3aa424d60a3de2d707fe0
oonderstanbable <45098109+oonderstanbable@users.noreply.github.com>
2018-12-20 17:39:44 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -24,64 +24,47 @@ function tL(a){return a.toLowerCase();}
 
 
 bot.on('message', message => {
-client.on('message', message => {
     if (tL(message.content) === 'ok google?') {
         message.reply('https://www.accuweather.com/');
-    }
-});
+    });
 
-client.on('message', message => {
     if (tL(message.content) === 'ok google when does OOOPS have a birthday?') {
         message.reply('OOOPS has a birthday on November 3rd every year... like your birthday should be...');
-    }
-});
+    });
     
-client.on('message', message => {
     if (tL(message.content) === 'ok google flip a coin') {
         message.reply(coinflip());
-    }
-});
+    });
     
-client.on('message', message => {
     if (tL(message.content) === 'ok google ball') {
         message.reply(ball());
-    }
-});
+    });
     
-client.on('message', message => {
     if (tL(message.content) === 'ok google gay?') {
         message.reply('Brandor is the gayest person in the world');
-    }
-});
+    });
  
-    client.on('message', message => {
     if (tL(message.content) === 'ok google twitter?') {
         message.reply('https://twitter.com/OOnderstandable');
-    }
-});
+    });
     
-client.on('message', message => {
+
     if (tL(message.content) === 'ok google why is OOOPS so bad a krunker?') {
         message.reply('Maybe he is bad but he is far better than you so if he is bad what does that make you... (hint FUCKING GARBAGE)');
-    }
-});
+    });
     
-client.on('message', message => {
     if (tL(message.content) === 'ok google') {
         message.channel.send('Hello, Google Home is at your service.');
-    }
-}); 
+    }); 
     
-client.on('message', message => {
+
     if (tL(message.content.slice(0,15)) === 'ok google urban') {
         urban(tL(message.content).slice(15)).then(function(value) {
         wordArray = JSON.parse(JSON.stringify(value));
         message.channel.send({"embed":{"color":15229474,"fields":[{"name":"Word:","value":cL(wordArray['word'])},{"name":"Definition:","value":cL(wordArray['definition'])},{"name":"Example:","value":cL(wordArray['example'])}]}});
         });
-    }
-});
+    });
 
-client.on('message', message => {
   // Voice only works in guilds, if the message does not come from a guild,
   // we ignore it
   if (!message.guild) return;
@@ -97,10 +80,8 @@ client.on('message', message => {
     } else {
       message.reply('You need to join a voice channel first!');
     }
-  }
-});
+  });
 
-client.on('message', message => {
     if (tL(message.content) === 'ok google help') {
         message.author.send("```CSS\n" 
         + "ok google\n"
@@ -115,8 +96,7 @@ client.on('message', message => {
         + "ok google why is OOOPS so bad a krunker?\n"
         + "ok google gay?\n"
         + "ok google twitter?```");
-    }
-});
+    });
 
 client.on("message",a=>{mV=a.content.split(" "),mV.length>=7&&"ok google what is"==tL(a.content.slice(0,17))&&iN(mV[4])&&iO(mV[5])&&iN(mV[6])&&a.channel.send(Math.round(1e4*mC[mV[5]](parseInt(mV[4]),parseInt(mV[6])))/1e4)});
 
