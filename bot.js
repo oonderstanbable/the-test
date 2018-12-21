88e551c97eae332ff8ad2c0ea7e1d3b387dd8662
oonderstanbable <45098109+oonderstanbable@users.noreply.github.com>
2018-12-20 18:05:21 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const urban = require('urban.js');
 
 client.on('ready', () => {
     console.log('Google Home!');
-    //client.user.setActivity('for help | ok google help');
+    //bot.user.setActivity('Google Home is here!');
 });
 
 
@@ -23,6 +23,25 @@ client.on('message', message => {
     if (tL(message.content) === 'ok google') {
         message.channel.send('Hello, Google Home is at your service.');
     }
+});
+
+client.on('message', message => {
+  // Voice only works in guilds, if the message does not come from a guild,
+  // we ignore it
+  if (!message.guild) return;
+
+  if (message.content === 'ok google join vc') {
+    // Only try to join the sender's voice channel if they are in one themselves
+    if (message.member.voiceChannel) {
+      message.member.voiceChannel.join()
+        .then(connection => { // Connection is an instance of VoiceConnection
+          message.reply('I have successfully connected to the channel!');
+        })
+        .catch(console.log);
+    } else {
+      message.reply('You need to join a voice channel first!');
+    }
+  }
 });
 
 client.on('message', message => {
