c9c1e128ff5732e4815350e26611482341696ca1
oonderstanbable <45098109+oonderstanbable@users.noreply.github.com>
2018-12-22 18:02:35 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -24,6 +24,21 @@ client.on('message', message => {
 });
 
 client.on('message', message => {
+  if (!message.guild) return;
+  if (message.content === 'ok google join vc') {
+    if (message.member.voiceChannel) {
+      message.member.voiceChannel.join()
+        .then(connection => { 
+          message.reply('I have successfully connected to the channel!');
+        })
+        .catch(console.log);
+    } else {
+      message.reply('You need to join a voice channel first!');
+    }
+  }
+});
+
+client.on('message', message => {
     if (tL(message.content) === 'ok google what is the weather like today?') {
         message.reply('https://www.accuweather.com/');
     }
